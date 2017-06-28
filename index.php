09b8831407bdb35f56d7d468911eedc34d9781c6
maromo71 <professormoraes@gmail.com>
2017-06-28 15:17:29 -0300

    Maromo mudança

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-    
-    </body>
-</html>
+<?php
+    $a = [1];
+    $a = [2];
+
+   foreach ($a as $elemento ) {
+       printf($elemento);
+   }
